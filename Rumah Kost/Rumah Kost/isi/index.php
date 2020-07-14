e7c4e8e05a847be2ccef7d73ad8ca325471ac895
Mulyaningsih <56246353+Mulyaningsih@users.noreply.github.com>
2020-07-14 21:45:38 +0700

    Add files via upload

---
+++ Rumah Kost/Rumah Kost/isi/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>HOMEL</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+   <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">KOSAN MELATI</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
+        <li class="dropdown">
+         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">MENU KOS<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="page/Tipe.php">Tipe</a></li>
+            <li><a href="page/Lokasi.php">Lokasi</a></li>
+            <li role="separator" class="divider"></li>
+          </li>
+          </ul>
+      </ul>
+    </option>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid atas">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 text-atas">
+        <h1>KOSAN MELATI WEB</h1>
+        <p></p>
+      </div>
+      <div class="col-md-6">
+       <div class="embed-responsive embed-responsive-16by9">
+       <iframe class="embed-responsive-item" src="media/2.jpg"></iframe>
+       </div>  
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+ 
+  <div class="col-md-4">
+    <div class="panel panel-default">
+     <div class="panel-body text-center">
+       <h2>Nama kelompok</h2>
+       <p>Nurul Hidayati</p>
+     <p>Viona Paramitha</p>
+     <p>Oktavia</p>
+     </div>
+    </div>
+  </div>
+  </div>
+</div>
+</div>
+
+
+    <script src="js/jqueryo.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ Rumah Kost/Rumah Kost/isi/page/Lokasi.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>LOKASI</title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+  </head>
+  <body>
+   <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">KOSAN MELATI</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="../index.php">Home<span class="sr-only">(current)</span></a></li>
+        <li class="dropdown">
+         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">MENU KOS<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+           <li><a href="Tipe.php">Tipe</a></li>
+            <li class="active"><a href="#">Lokasi</a></li>
+            <li role="separator" class="divider"></li>
+         </ul>
+        </li>
+      </ul>
+    </option>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+        </div>
+      </form>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+  <div class="row">
+   <div class="col-sm-12 text-center">
+     <h1><span class="glyphicon glyphicon-cup" aria-hidden="true"></span>KOSAN MELATI</h1> 
+   </div>
+  </div>
+ <div class="row">
+  <div class="col-md-8 col-sm-6">
+    <div class="panel panel-default">
+     <div class="panel-body text-center">
+       <a href=""><img src="../media/1.jpg" class="img-responsive"  align="center"></a>
+        <h2 class="button"></h2>
+      <a class="btn btn-primary" target="_blank" href="https://www.google.co.id/maps/place/Politeknik+Harapan+Bersama/@-6.8685733,109.1050177,17z/data=!4m5!3m4!1s0x2e6fb9e2805c1c1b:0xe3e61e1ae59106ff!8m2!3d-6.8685786!4d109.1072064/">LOKASI</a>
+     </div>
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../js/jqueryo.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
